f3c2abcda3ed8623b7d9588e6766c2f608f2904e
Chris Eichelberger <chris@eichelberger.org>
2019-10-27 19:05:10 -0400

    need to complete the orientation-transition table
    Signed-off-by: Chris Eichelberger <chris@eichelberger.org>

--- zorder/src/main/scala/org/locationtech/sfcurve/zorder/TriN.scala
+++ zorder/src/main/scala/org/locationtech/sfcurve/zorder/TriN.scala
@@ -28,7 +28,20 @@ import org.locationtech.sfcurve.Dimensions.{Cell, Extent, Latitude, Longitude}
 
 import TriN._
 
-case class Triangle(index: Long, orientation: Int, X: Extent[Double], Y: Extent[Double]) {
+case class Triangle(index: Long, orientation: Int, X: Extent[Double], Y: Extent[Double], depth: Int) {
+  val xMid: Double = 0.5 * (X.min + X.max)
+
+  def bitString: String = index.toBinaryString.reverse.padTo(3 * depth, "0").reverse.mkString("")
+
+  def isApexUp: Boolean = orientation match {
+    case 0 | 1 | 2 | 3 | 4 | 5 => true
+    case _ => false
+  }
+
+  def stackIndex(nextIndex: Long): Long = (index << 3) | (nextIndex & 0x111)
+
+  def nextOrientation(nextIndex: Int): Int = OrientationTransitions(orientation)(nextIndex)
+
   def getTriangle(x: Double, y: Double, maxDepth: Int): Triangle = {
     if (maxDepth < 1) return this
     
@@ -36,13 +49,35 @@ case class Triangle(index: Long, orientation: Int, X: Extent[Double], Y: Extent[
     val x1 = X.max
     val dx = x1 - x0
     val x14 = x0 + 0.25 * dx
-    val xmid = x0 + 0.50 * dx
     val x34 = x0 + 0.75 * dx
 
     val y0 = Y.min
     val y1 = Y.max
     val dy = y1 - y0
-    val ymid = y0 + 0.50 * dy
+    val yMid = y0 + 0.50 * dy
+
+    if (isApexUp) {
+      // top
+      if (y > yMid) {
+        return Triangle(stackIndex(IndexTop), nextOrientation(IndexTop), Extent(x14, x34), Extent(yMid, Y.max), depth + 1).getTriangle(x, y, maxDepth - 1)
+      }
+      // lower left
+      val b0 = (xMid * yMid) / (xMid - x14) + y0 * x14
+      val m0 = (y0 - b0) / xMid
+      if (y < (m0 * x + b0)) {
+        return Triangle(stackIndex(IndexLL), nextOrientation(IndexLL), Extent(x0, xMid), Extent(y0, yMid), depth + 1).getTriangle(x, y, maxDepth - 1)
+      }
+      // lower right
+      val m1 = (yMid - y0) / (x1 - xMid)
+      val b1 = y0 - m1 * xMid
+      if (y < (m1 + x * b1)) {
+        return Triangle(stackIndex(IndexLR), nextOrientation(IndexLR), Extent(xMid, x1), Extent(y0, yMid), depth + 1).getTriangle(x, y, maxDepth - 1)
+      }
+      // middle
+      return Triangle(stackIndex(IndexCenter), nextOrientation(IndexCenter), Extent(x14, x34), Extent(y0, yMid), depth + 1).getTriangle(x, y, maxDepth - 1)
+    } else {
+
+    }
 
     val iy: Int = if (y >= 0.5 * (Y.min + Y.max)) 1 else 0
       
@@ -51,12 +86,122 @@ case class Triangle(index: Long, orientation: Int, X: Extent[Double], Y: Extent[
     // TODO!
     null
   }
+
+  def polygonPoints: String = orientation match {
+    case 0 | 1 | 2 | 3 | 4 | 5 =>
+      // apex on top
+      s"${xMid} ${Y.max}, ${X.max} ${Y.min}, ${X.min} ${Y.min}, ${xMid} ${Y.max}"
+    case _ =>
+      // apex on bottom
+      s"${X.min} ${Y.max}, ${X.max} ${Y.max}, ${xMid} ${Y.min}, ${X.min} ${Y.max}"
+  }
+
+  def wkt: String = {
+    "POLYGON((" + polygonPoints + "))"
+  }
 }
 
 object TriN {
   val Sqrt3: Double = Math.sqrt(3.0)
   val TriTopLongitude = Longitude(4)
   val TriTopLatitude = Latitude(2)
+
+  val IndexTop = 1
+  val IndexCenter = 0
+  val IndexLL = 4
+  val IndexLR = 2
+
+  // possible triangle orientations...
+  // apex up
+  val A_BC = 0
+  val B_CA = 1
+  val C_AB = 2
+  val A_CB = 3
+  val B_AC = 4
+  val C_BA = 5
+  // apex down
+  val AB_C = 6
+  val BC_A = 7
+  val CA_B = 8
+  val AC_B = 9
+  val BA_C = 10
+  val CB_A = 11
+
+  val OrientationTransitions: Map[Int, Map[Int, Int]] = Map(
+    A_BC -> Map(
+      IndexTop -> A_CB,
+      IndexLL -> B_AC,
+      IndexLR -> C_BA,
+      IndexCenter -> BC_A
+    ),
+    // TODO:  the following are UNVETTED!
+    B_CA -> Map(
+      IndexTop -> A_CB,
+      IndexLL -> B_AC,
+      IndexLR -> C_BA,
+      IndexCenter -> BC_A
+    ),
+    C_AB -> Map(
+      IndexTop -> A_CB,
+      IndexLL -> B_AC,
+      IndexLR -> C_BA,
+      IndexCenter -> BC_A
+    ),
+    A_CB -> Map(
+      IndexTop -> A_CB,
+      IndexLL -> B_AC,
+      IndexLR -> C_BA,
+      IndexCenter -> BC_A
+    ),
+    B_AC -> Map(
+      IndexTop -> A_CB,
+      IndexLL -> B_AC,
+      IndexLR -> C_BA,
+      IndexCenter -> BC_A
+    ),
+    C_BA -> Map(
+      IndexTop -> A_CB,
+      IndexLL -> B_AC,
+      IndexLR -> C_BA,
+      IndexCenter -> BC_A
+    ),
+    AB_C -> Map(
+      IndexTop -> A_CB,
+      IndexLL -> B_AC,
+      IndexLR -> C_BA,
+      IndexCenter -> BC_A
+    ),
+    BC_A -> Map(
+      IndexTop -> A_CB,
+      IndexLL -> B_AC,
+      IndexLR -> C_BA,
+      IndexCenter -> BC_A
+    ),
+    CA_B -> Map(
+      IndexTop -> A_CB,
+      IndexLL -> B_AC,
+      IndexLR -> C_BA,
+      IndexCenter -> BC_A
+    ),
+    AC_B -> Map(
+      IndexTop -> A_CB,
+      IndexLL -> B_AC,
+      IndexLR -> C_BA,
+      IndexCenter -> BC_A
+    ),
+    BA_C -> Map(
+      IndexTop -> A_CB,
+      IndexLL -> B_AC,
+      IndexLR -> C_BA,
+      IndexCenter -> BC_A
+    ),
+    CB_A -> Map(
+      IndexTop -> A_CB,
+      IndexLL -> B_AC,
+      IndexLR -> C_BA,
+      IndexCenter -> BC_A
+    )
+  )
 
   // map the given (x, y) coordinate to one face of the octahedron
   // that wraps the entire (spherical) Earth
@@ -72,7 +217,7 @@ object TriN {
     val idx: Long = (iy << 2) | (if (ix < 2) ix else 5 - ix)
     val x0: Double = -180.0 + 90.0 * ix
     val y0: Double = -90.0 + 90.0 * iy
-    Triangle(idx, orientation, Extent(x0, x0 + 90.0), Extent(y0, y0 + 90.0))
+    Triangle(idx, orientation, Extent(x0, x0 + 90.0), Extent(y0, y0 + 90.0), 1)
   }
 
   def getXT(x: Double, y: Double, X: Extent[Double]): Double = {
@@ -174,6 +319,16 @@ object TriTest extends App {
 //    testInitialTri(5, 179.9999, 45.0)
 //    testInitialTri(6, 0.0, 89.9999)
 //    testInitialTri(7, -78.4767, 38.0293)
+    pw.close()
+
+    pw = new PrintWriter(new FileWriter("test-index.txt"))
+    pw.println("depth\tindex_dec\tindex_bits\twkt")
+    (1 to 5).foldLeft((-78.4767, 38.0293))((acc, depth) => acc match {
+      case (x, y) =>
+        val t = getTriangle(-78.4767, 38.0293, depth)
+        pw.println(depth + "\t" + t.index + "\t" + t.bitString + "\t" + t.wkt)
+        (x, y)
+    })
   } finally {
     pw.close()
   }
